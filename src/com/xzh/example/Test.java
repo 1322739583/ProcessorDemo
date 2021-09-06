eebfdca813b75d28211d94ee690c7a010784f8eb
1322739583 <1322739583@qq.com>
2021-09-06 14:42:40 +0800

    Initial commit

---
+++ src/com/xzh/example/Test.java
@@ -0,0 +1,9 @@
+package com.xzh.example;
+
+
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/xzh/example/builders/Car.java
@@ -0,0 +1,34 @@
+package com.xzh.example.builders;
+
+
+import com.xzh.example.Builder;
+
+@Builder
+public class Car extends Vehicle {
+
+    public enum FuelType{
+        ELECTRIC,
+        GAS,
+        DIESEL
+    }
+    public boolean isAutomatic;
+    public FuelType fuelType;
+
+    Car(String make, String model, String year,boolean isAutomatic,FuelType fuelType) {
+        super(make, model, year);
+        this.isAutomatic = isAutomatic;
+        this.fuelType = fuelType==null? FuelType.GAS:fuelType;
+    }
+
+    public Car(){
+        super(null,null,null);
+    }
+
+
+    interface CarBuilder<T>{
+        T setIsAutomatic(boolean isAutomatic);
+        T setFuelType(FuelType fuelType);
+        Car buildCar();
+    }
+
+}
